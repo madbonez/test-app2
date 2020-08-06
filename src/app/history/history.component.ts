68ae235427008a72f979068914d88afff7504032
artem <amalov@reinform.ru>
2020-08-07 00:40:41 +0300

    adaptive columns

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,5 +1,6 @@
-import {ChangeDetectionStrategy, Component, HostBinding, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, HostBinding, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import {BreakpointObserver} from '@angular/cdk/layout';
 
 const ELEMENT_DATA: any[] = [
   {index: 1, cardSender: '1234123412341234', cardReceiver: '1234123412341234', total: 5000, date: 'date'},
@@ -29,15 +30,25 @@ const ELEMENT_DATA: any[] = [
 export class HistoryComponent implements OnInit {
   displayedColumns: string[] = ['index', 'cardSender', 'cardReceiver', 'total', 'date', 'action'];
   dataSource = ELEMENT_DATA;
+  private isSmallScreen: boolean;
 
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver, private cd: ChangeDetectorRef) { }
 
   @HostBinding('@slideUpDown') get value(): boolean {
     return true;
   }
 
   ngOnInit(): void {
+    this.breakpointObserver.observe('(max-width: 704px)').subscribe(val => {
+      this.isSmallScreen = val.matches;
+      if (this.isSmallScreen) {
+        this.displayedColumns = ['total', 'date', 'action'];
+      } else {
+        this.displayedColumns =  ['index', 'cardSender', 'cardReceiver', 'total', 'date', 'action'];
+      }
+      this.cd.markForCheck();
+    });
   }
 
 }
